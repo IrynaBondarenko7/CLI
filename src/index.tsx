c8a8ba508656a5eab71d90c175e37699ee8b7d1b
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-12-27 19:10:09 +0000

    added monaco editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugins";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
+
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
@@ -65,6 +68,10 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor
+        initialValue="const a =1;"
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
